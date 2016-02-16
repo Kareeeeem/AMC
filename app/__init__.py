87e5d99012a7940d941e48e6567d990f188a17d7
Mohammed Kareem <kareeeeem@gmail.com>
2016-02-16 16:16:12 +0100

    converter now assigined to app in FlaskIntEncoder

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,6 @@ def create_app(config_name):
 
     db.init_app(app)
     hashid.init_app(app)
-    app.url_map.converters['id'] = hashid.id_converter
 
     from app.api.v1 import auth
     app.register_blueprint(auth.bp)
