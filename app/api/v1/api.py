a9a977c39f7b8bd6ac41d2612be5facafb394131
Mohammed Kareem <kareeeeem@gmail.com>
2016-03-21 02:04:59 +0100

    thinking out api endpoints

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -7,6 +7,43 @@ from . import v1
 GET, PUT, POST, DEL = 'GET', 'PUT', 'POST', 'DELETE'
 
 
+# USER ENDPOINTS
+# ==============
+# /users/registration                          POST   register with the app
+# /users                                       GET    retrieve all users
+# /users/<id>                                  GET    retrieve a single user
+# /users/<id>                                  PUT    edit user
+# /users/<id>                                  DELETE edit user
+# /users/<id>/exercises                        GET    retreive all exercises authored by user
+# /users/<id>/ratings                          GET    retreive all ratings authored by user
+# /users/<id>/responses                        GET    retreive all responses authored by user
+
+# EXERCISE ENDPOINTS
+# ==================
+# /exercises                                   GET    retrieve all exercises
+# /exercises                                   POST   new exercise
+# /exercises/<id>                              GET    retrieve a single exercise
+# /exercises/<id>                              PUT    edit exercise
+# /exercises/<id>                              DELETE delete exercise
+# /exercises/<id>/ratings                      GET    all ratings for exercise
+# /exercises/<id>/ratings                      POST   new rating
+# /exercises/<id>/ratings/<user_id>            GET    retrieve rating
+# /exercises/<id>/ratings/<user_id>            PUT    edit rating
+# /exercises/<id>/ratings/<user_id>            DELETE delete rating
+
+# QUESTIONNAIRE ENDPOINTS
+# ==================
+# /questionnaires                              GET  retrieve all questionnaires
+# /questionnaires                              POST new questionnaire
+# /questionnaires/<id>                         GET  retrieve a single questionnaire
+# /questionnaires/<id>                         PUT  edit questionnaire
+# /questionnaires/<id>                         DELETE  delete questionnaire
+# /questionnaires/<id>/responses               GET  retrieve all response
+# /questionnaires/<id>/responses               POST new response
+# /questionnaires/<id>/responses/<response_id> GET  get response
+# /questionnaires/<id>/responses/<response_id> PUT  edit response
+# /questionnaires/<id>/responses/<response_id> DELETE  delete response
+
 @v1.errorhandler(ValidationError)
 def validation_error(exception=None):
     return dict(errors=exception.messages), 400
@@ -33,6 +70,12 @@ def users(id):
     schema = serializers.UserSchema()
     rv = schema.dump(user).data
     return rv
+
+
+@v1.route('/users/<hashid:id>/exercises')
+def user_exercises(id):
+    '''All exercises authored by user.'''
+    return {}
 
 
 @v1.route('/exercises')
