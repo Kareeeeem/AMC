8aab4984477b5b7c173b4440f2f2896f00809438
Mohammed Kareem <kareeeeem@gmail.com>
2016-03-21 02:04:31 +0100

    no need for localproxy

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,13 +1,10 @@
 from flask import request, url_for
 from marshmallow import ValidationError
-from werkzeug.local import LocalProxy
 
 from app import models, db, auth, serializers
-
 from . import v1
 
 GET, PUT, POST, DEL = 'GET', 'PUT', 'POST', 'DELETE'
-get_json = LocalProxy(lambda: request.get_json)
 
 
 @v1.errorhandler(ValidationError)
@@ -20,7 +17,7 @@ def registration():
     '''Entry point for registration.
     '''
     schema = serializers.UserSchema()
-    user_data = schema.load(get_json()).data
+    user_data = schema.load(request.get_json()).data
 
     user = models.User(**user_data)
     db.session.add(user)
